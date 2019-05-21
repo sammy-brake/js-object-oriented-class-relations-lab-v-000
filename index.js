17acaa517408ebe0d26fbe0d4794cb8c0e6cd85e
Samantha brake <sammy.brake@gmail.com>
2019-05-21 18:41:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let driverId = 0;
 let passengerId = 0;
 let tripId = 0;
 let store = {drivers: [], passengers: [], trips: []}
+
 class Driver {
   constructor(name) {
     this.name = name;
@@ -17,6 +18,9 @@ class Passenger {
     this.name = name;
     this.id = ++passengerId;
       store.passengers.push(this);
+      if(driver){
+      this.setDriver(driver)
+    }
   }
 };
 
