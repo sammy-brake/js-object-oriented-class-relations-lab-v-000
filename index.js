abf1f948af44d7b0b448fc070c678f30a78bab7a
Samantha brake <sammy.brake@gmail.com>
2019-05-21 18:58:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,4 +46,9 @@ class Trip {
       return driver.id === this.driverId;
     });
   }
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
 };
